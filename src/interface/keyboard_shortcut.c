d51edbf70d1a101435fd1a3ada29ea19a79b2266
IntelOrca <ted@brambles.org>
2015-02-12 16:23:49 +0000

    add chat feature

--- src/interface/keyboard_shortcut.c
+++ src/interface/keyboard_shortcut.c
@@ -22,6 +22,7 @@
 #include "../config.h"
 #include "../game.h"
 #include "../input.h"
+#include "../network/network.h"
 #include "keyboard_shortcut.h"
 #include "viewport.h"
 #include "window.h"
@@ -415,6 +416,11 @@ static void shortcut_open_cheat_window()
 	window_cheats_open();
 }
 
+static void shortcut_open_chat_window()
+{
+	network_open_chat_box();
+}
+
 static const shortcut_action shortcut_table[SHORTCUT_COUNT] = {
 	shortcut_close_top_most_window,
 	shortcut_close_all_floating_windows,
@@ -450,7 +456,8 @@ static const shortcut_action shortcut_table[SHORTCUT_COUNT] = {
 	shortcut_screenshot,
 	shortcut_reduce_game_speed,
 	shortcut_increase_game_speed,
-	shortcut_open_cheat_window
+	shortcut_open_cheat_window,
+	shortcut_open_chat_window
 };
 
 #pragma endregion
